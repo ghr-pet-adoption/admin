6100310caae5c36af625d3c60bda3632c83ceaef
marArcz <mar19arcz@gmail.com>
2022-09-15 23:48:32 +0800

    update

--- src/index.tsx
+++ src/index.tsx
@@ -15,7 +15,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename='/'>
+    <BrowserRouter basename='/admin/'>
       <App />
     </BrowserRouter>
   </React.StrictMode>
